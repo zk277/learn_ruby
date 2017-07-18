f84224518ddc98d4d7e902821e5615838f04d084
zk277 <zuzaka22@hotmail.com>
2017-07-18 10:42:58 +0100

    completed

--- 04_pig_latin/pig_latin_spec.rb
+++ 04_pig_latin/pig_latin_spec.rb
@@ -1,25 +1,28 @@
 # # Topics
-#
 # * modules
 # * strings
 #
 # # Pig Latin
+# Pig Latin is a made-up children's language that's intended to be confusing.
+# It obeys a few simple rules (below) but when it's spoken quickly
+# it's really difficult for non-children (and non-native speakers)
+# to understand.
 #
-# Pig Latin is a made-up children's language that's intended to be confusing. It obeys a few simple rules (below) but when it's spoken quickly it's really difficult for non-children (and non-native speakers) to understand.
+# Rule 1: If a word begins with a vowel sound, add an "ay" sound
+# to the end of the word.
 #
-# Rule 1: If a word begins with a vowel sound, add an "ay" sound to the end of the word.
+# Rule 2: If a word begins with a consonant sound, move it to the end
+# of the word, and then add an "ay" sound to the end of the word.
 #
-# Rule 2: If a word begins with a consonant sound, move it to the end of the word, and then add an "ay" sound to the end of the word.
-#
-# (There are a few more rules for edge cases, and there are regional variants too, but that should be enough to understand the tests.)
+# (There are a few more rules for edge cases, and there are regional variants
+# too, but that should be enough to understand the tests.)
 #
 # See <http://en.wikipedia.org/wiki/Pig_latin> for more details.
-#
 #
 
 require "pig_latin"
 
-describe "#translate" do
+describe "translate" do
 
   it "translates a word beginning with a vowel" do
     s = translate("apple")
@@ -66,7 +69,18 @@ describe "#translate" do
   end
 
   # Test-driving bonus:
-  # * write a test asserting that capitalized words are still capitalized (but with a different initial capital letter, of course)
+  # * write a test asserting that capitalized words are still capitalized
+  # (but with a different initial capital letter, of course)
   # * retain the punctuation from the original phrase
+
+  it "keeps capitalized words capitalized" do
+    s = translate ("Happy")
+    expect(s).to eq("Appyhay")
+  end
+
+  it "retains the puctuation from original phrase" do
+    s = translate ("The quick, brown fox.")
+    expect(s).to eq("Ethay ickquay, ownbray oxfay.")
+  end
 
 end
